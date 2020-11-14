6e8d81af889733d2b8ed996c1247d47dc903f39f
Naty <natylandrobe@gmail.com>
2020-11-14 19:56:12 -0300

    Add project structure: inputs, data and graph

---
+++ tp.py
@@ -0,0 +1,43 @@
+import pandas as pd 
+import scipy.signal as sig
+import matplotlib.pyplot as plt 
+
+#Loop para preguntar la edad hasta que se ingrese una válida
+while True:
+    age = input("Por favor ingrese la edad del paciente en años:\n")
+    if not age.isnumeric():
+        print("Edad invalida")
+        continue
+    age = int(age)
+    if(age < 0 or age > 150):
+        print("Edad invalida")
+        continue
+    break
+
+#Loop para preguntar el sexo hasta obtener uno válido
+while True:
+    sex = input("Por favor ingrese su sexo (M o F):\n")
+    sex = sex.upper()
+    if (sex != "M") and (sex != "F"):
+        print("Sexo inválido")
+        continue
+    break
+
+#Leer el archivo del ECG y guardarlo en un dataframe
+ecg = pd.read_excel("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Clase_4_datos/electrocardiograma.xlsx")
+
+#Busco los picos de mayor importancia
+peak_index, _ = sig.find_peaks(ecg["señal"], prominence = 1)
+#Tomo los valores en los picos
+peaks = ecg["señal"].iloc[peak_index]
+#Tomo los tiempos en los que se produjeron los picos
+peak_times = ecg["tiempo"].iloc[peak_index]
+
+
+#Grafico
+plt.plot(peak_times, peaks, 'go', label = "Picos")
+plt.plot(ecg["tiempo"], ecg["señal"])
+plt.xlabel("s")
+plt.ylabel("eV")
+plt.legend()
+plt.show()
